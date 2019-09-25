4ed1a17e9ff2a218b8c93c4941730b155ce2199f
MikeStegall <mestegall5@gmail.com>
2019-09-24 21:24:09 -0500

    added a logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
-        <nav style={{display:'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-        </nav>
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3"><img style={{paddingTop: '5px'}} alt='logo' src={brain}/></div>
+            </Tilt>
+        </div>
     )
 }
 
