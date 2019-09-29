9a7e9d1f3e3a00173176c72c8d007d69faebac2f
MikeStegall <mestegall5@gmail.com>
2019-09-28 19:08:41 -0500

    added particles.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Navigation from './Components/Navigation/Navigation'
-import Logo from './Components/Logo/Logo'
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 175,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' 
+                params={particlesOptions}
+      />
       <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRegognition /> */}
     </div>
